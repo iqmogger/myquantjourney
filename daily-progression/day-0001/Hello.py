ba7edc2645456e77ae909137181ae7c8cc03ec58
ST3PH3N <stephenrsmain@gmail.com>
2026-04-12 18:53:29 -0700

    Add Hello.py with initial Python learning prints

---
+++ daily-progression/day-0001/Hello.py
@@ -0,0 +1,8 @@
+# The # symbol means comment so it ignores it
+# Will be explaining code and what I learn like this
+
+print("hello, quant world") 
+print("today is day 0001")
+print("i am learning python from zero")
+
+# These just print whatever is in there - I already know this.
