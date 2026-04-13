3db754d33c56043ba977ad41e77aba27850624d3
ST3PH3N <stephenrsmain@gmail.com>
2026-04-12 18:58:22 -0700

    Add initial variables for stock calculations
    Initialized variables for greeting, stock ticker, and financial calculations.

---
+++ daily-progression/day-0001/variables.py
@@ -0,0 +1,25 @@
+# strings
+greeting = "hello"
+my_name = "Stephen"
+ticker = "AAPL"
+
+# Just saving variables. Easy, I know this.
+
+
+# numbers
+ROI = 1000000
+shares = 10 # Ints - whole numbers
+price = 34.50 # Float - has a decimal
+
+# I also know this.
+
+
+# math
+total_value = price * shares   # multiply: 34.50 × 10
+cost_basis = 550.00
+profit = total_value - (cost_basis * shares)
+
+# Common sense
+
+
+# f-strings
