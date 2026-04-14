c587d7e882b8fabce6e68b86e57ef89cc3a0d708
ST3PH3N <stephenrsmain@gmail.com>
2026-04-14 11:54:11 -0700

    Update rlng-wndws-moving-avg.py

--- daily-progression/day-0003/rlng-wndws-moving-avg.py
+++ daily-progression/day-0003/rlng-wndws-moving-avg.py
@@ -13,7 +13,7 @@ apple['SMA_20'] = apple['Close'].rolling(window=20).mean()
 # Notation explained:
 # apple['SMA_20'] is creating a new column
 # .rolling(window=n) is basically like a sliding window that moves through the data one row at a time.
-# What it does is it basicvally gives you the average return every n days. 
+# What it does is it basicvally gives you the average price every n days. 
 
 # This creates a NEW column where each value is the average of the last 20 closes
 print(apple[['Close', 'SMA_20']].head(25))
