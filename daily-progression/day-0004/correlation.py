f87907bef507319cfb858f83c818cd1ad5d3df5b
ST3PH3N <stephenrsmain@gmail.com>
2026-04-15 23:39:11 -0700

    Create correlation.py

---
+++ daily-progression/day-0004/correlation.py
@@ -0,0 +1,14 @@
+import yfinance as yf
+import pandas as pd
+
+tickers = ['AAPL', 'MSFT', 'TSLA', 'SPY']
+
+data = yf.download(tickers, start='2024-01-01', end='2025-01-01')
+
+returns = closes.pct_change()
+print(returns.head())
+# Every column calculated at the same time
+
+corr_matrix = returns.corr() # Simple notation. Just 
+print(corr_matrix)
+
