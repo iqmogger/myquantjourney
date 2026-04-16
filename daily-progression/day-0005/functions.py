99ded3b3bf4dc5684adad38ab2a9d220728bc1c6
ST3PH3N <stephenrsmain@gmail.com>
2026-04-16 10:24:29 -0700

    Add function to sum two numbers and print result

---
+++ daily-progression/day-0005/functions.py
@@ -0,0 +1,6 @@
+def add(a, b):        # Def means "define (a function"  ;  add is the name   ;   (a, b) is the argument
+    result = a + b      #tells it what to do
+    return result      # important part: must write "return results"
+
+answer = add(3, 5)
+print(answer)    #its 8
