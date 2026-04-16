9fb1ccd07f56b57213de53b433bcca1c40633c5b
ST3PH3N <stephenrsmain@gmail.com>
2026-04-15 23:49:50 -0700

    Update correlation.py

--- daily-progression/day-0004/correlation.py
+++ daily-progression/day-0004/correlation.py
@@ -5,6 +5,8 @@ tickers = ['AAPL', 'MSFT', 'TSLA', 'SPY']
 
 data = yf.download(tickers, start='2024-01-01', end='2025-01-01')
 
+closes = data['Close']
+
 returns = closes.pct_change()
 print(returns.head())
 # Every column calculated at the same time
