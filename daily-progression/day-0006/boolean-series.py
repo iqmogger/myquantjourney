7b873790f2127ab640f3e2c816a7ef203cad2c96
ST3PH3N <stephenrsmain@gmail.com>
2026-04-17 10:23:25 -0700

    Create boolean-series.py

---
+++ daily-progression/day-0006/boolean-series.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import yfinance as yf
+
+data = yf.download('AAPL', start='2024-01-01', end='2025-01-01', auto_adjust=True)
+data.columns = data.columns.droplevel(1)  # 
+
+# Calculate a 20-day moving average (you know this from Day 0003)
+data['SMA_20'] = data['Close'].rolling(window=20).mean()
+
+# Now the new part — create a Series of True/False values
+above_sma = data['Close'] > data['SMA_20']
+print(above_sma.head(25))
