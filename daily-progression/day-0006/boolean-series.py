f162a5853789023a69fff30c9e24a2b690c913d9
ST3PH3N <stephenrsmain@gmail.com>
2026-04-17 10:24:33 -0700

    Update boolean-series.py

--- daily-progression/day-0006/boolean-series.py
+++ daily-progression/day-0006/boolean-series.py
@@ -2,7 +2,7 @@ import pandas as pd
 import yfinance as yf
 
 data = yf.download('AAPL', start='2024-01-01', end='2025-01-01', auto_adjust=True)
-data.columns = data.columns.droplevel(1)  # 
+data.columns = data.columns.droplevel(1)  # droplevel basically gets rid of row 2. It counts from 0.
 
 # Calculate a 20-day moving average (you know this from Day 0003)
 data['SMA_20'] = data['Close'].rolling(window=20).mean()
